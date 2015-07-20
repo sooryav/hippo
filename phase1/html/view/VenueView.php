75e8534b80a834ca3bbf9e8b466724fec5382a5c
Terry Kim <yuminkim@gmail.com>
2015-07-19 17:00:32 -0700

    Use bootstrap-xhp elements. Create a new element for displaying venue-list. Clean up javascript and more.

--- phase1/html/view/VenueView.php
+++ phase1/html/view/VenueView.php
@@ -5,18 +5,23 @@ require_once(__DIR__ . '/lib/composer/vendor/autoload.php');
 
 class :ui:venues:view extends :x:page:view {
 
+  // TODO: This may not be the best way to communicate
+  // to the client. Come up with something else?
   attribute int totalVenuesCount @required;
 
   <<Override>>
   public function getBody(): :x:frag {
     return
       <x:frag>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js" />
-        <script src="../javascript/vendor.js" />
-        <div id="results"></div>
-        <button class="load_more" id="load_more_button">load More</button>
-        <div class="animation_image" style="display:none;">Loading...</div>
-        <div id="totalVenuesCount" style="display:none;">
+        <script src="../js/vendor.js" />
+        <bootstrap:root>
+          <bootstrap:container>
+            <bootstrap:container id="results" />
+            <bootstrap:button use="primary" id="load_button">Load More</bootstrap:button>
+          </bootstrap:container>
+        </bootstrap:root>
+
+        <div id="totalVenuesCount" class="hidden">
           { $this->getAttribute('totalVenuesCount') }
         </div>
       </x:frag>;
