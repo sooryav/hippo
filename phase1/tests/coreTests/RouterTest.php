95e10077c19705790504cb5082db50525ccceadb
Terry Kim <yuminkim@gmail.com>
2015-08-04 00:16:53 -0700

    Router config generator tool.

--- phase1/tests/coreTests/RouterTest.php
+++ phase1/tests/coreTests/RouterTest.php
@@ -7,7 +7,7 @@ require_once(__DIR__ . '/../mock/MockLogger.php');
 
 class RouterTest extends \PHPUnit_Framework_TestCase {
 
-  public function testRouterWithUnknownKey() {
+  public function testRouterWithUnknownKey(): void {
     $router = new \Core\Router(ImmMap{});
     $request = new \Core\Request("foo", new \Core\RequestParams(Map{}));
     $context = new \Core\Context(
